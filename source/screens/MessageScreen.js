cd8d9f83713743f1502698e893215e875237290f
waleed826 <waleedbinakbar@gmail.com>
2024-02-13 19:18:34 +0500

    profile_Screen

--- source/screens/MessageScreen.js
+++ source/screens/MessageScreen.js
@@ -1,137 +1,137 @@
-import React, { useCallback, useState, useLayoutEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-// import { Avatar } from 'react-native-elements';
-import {auth, signOut,db } from '@react-native-firebase/auth'
-// import { auth, db } from '../firebase';
-// import { signOut } from 'firebase/auth';
-import { collection, addDoc, getDocs, query, orderBy, onSnapshot } from '@react-native-firebase/firestore';
-import { GiftedChat, Message } from 'react-native-gifted-chat';
+// import React, { useCallback, useState, useLayoutEffect } from 'react';
+// import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+// // import { Avatar } from 'react-native-elements';
+// import {auth, signOut,db } from '@react-native-firebase/auth'
+// // import { auth, db } from '../firebase';
+// // import { signOut } from 'firebase/auth';
+// import { collection, addDoc, getDocs, query, orderBy, onSnapshot } from '@react-native-firebase/firestore';
+// import { GiftedChat, Message } from 'react-native-gifted-chat';
 
-const MessageScreen = ({ navigation }) => {
-    const [messages, setMessages] = useState([]);
-    const signOutNow = () => {
-        signOut(auth).then(() => {
-            // Sign-out successful.
-            navigation.replace('Login');
-        }).catch((error) => {
-            // An error happened.
-        });
-    }
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerLeft: () => (
-                <View style={{ marginLeft: 20 }}>
-                    <Avatar
-                        rounded
-                        source={{
-                            uri: auth?.currentUser?.photoURL,
-                        }}
-                    />
-                </View>
-            ),
-            headerRight: () => (
-                <TouchableOpacity style={{
-                    marginRight: 10
-                }}
-                    onPress={signOutNow}
-                >
-                    <Text>logout</Text>
-                </TouchableOpacity>
-            )
-        })
+// const MessageScreen = ({ navigation }) => {
+//     const [messages, setMessages] = useState([]);
+//     const signOutNow = () => {
+//         signOut(auth).then(() => {
+//             // Sign-out successful.
+//             navigation.replace('Login');
+//         }).catch((error) => {
+//             // An error happened.
+//         });
+//     }
+//     useLayoutEffect(() => {
+//         navigation.setOptions({
+//             headerLeft: () => (
+//                 <View style={{ marginLeft: 20 }}>
+//                     <Avatar
+//                         rounded
+//                         source={{
+//                             uri: auth?.currentUser?.photoURL,
+//                         }}
+//                     />
+//                 </View>
+//             ),
+//             headerRight: () => (
+//                 <TouchableOpacity style={{
+//                     marginRight: 10
+//                 }}
+//                     onPress={signOutNow}
+//                 >
+//                     <Text>logout</Text>
+//                 </TouchableOpacity>
+//             )
+//         })
 
-        const q = query(collection(db, 'chats'), orderBy('createdAt', 'desc'));
-        const unsubscribe = onSnapshot(q, (snapshot) => setMessages(
-            snapshot.docs.map(doc => ({
-                _id: doc.data()._id,
-                createdAt: doc.data().createdAt.toDate(),
-                text: doc.data().text,
-                user: doc.data().user,
-            }))
-        ));
+//         const q = query(collection(db, 'chats'), orderBy('createdAt', 'desc'));
+//         const unsubscribe = onSnapshot(q, (snapshot) => setMessages(
+//             snapshot.docs.map(doc => ({
+//                 _id: doc.data()._id,
+//                 createdAt: doc.data().createdAt.toDate(),
+//                 text: doc.data().text,
+//                 user: doc.data().user,
+//             }))
+//         ));
 
-        return () => {
-          unsubscribe();
-        };
+//         return () => {
+//           unsubscribe();
+//         };
 
-    }, [navigation]);
+//     }, [navigation]);
 
-    const onSend = useCallback((messages = []) => {
-        const { _id, createdAt, text, user,} = messages[0]
+//     const onSend = useCallback((messages = []) => {
+//         const { _id, createdAt, text, user,} = messages[0]
 
-        addDoc(collection(db, 'chats'), { _id, createdAt,  text, user });
-    }, []);
+//         addDoc(collection(db, 'chats'), { _id, createdAt,  text, user });
+//     }, []);
 
-    return (
-        <GiftedChat
-            messages={messages}
-            showAvatarForEveryMessage={true}
-            onSend={messages => onSend(messages)}
-            user={{
-                _id: auth?.currentUser?.email,
-                name: auth?.currentUser?.displayName,
-                avatar: auth?.currentUser?.photoURL
-            }}
-        />
-    );
-}
-
-export default MessageScreen;
-
-// import { StyleSheet, Text, View } from 'react-native'
-// import React, { useCallback, useEffect, useState } from 'react'
-// import { GiftedChat } from 'react-native-gifted-chat'
-
-// const MessageScreen = () => {
-//   const [messages, setMessages] = useState([])
-//   useEffect(() => {
-//     setMessages([
-//       {
-//         _id: 1,
-//         name: 'React Native',
-//         text: 'Hello developer',
-//         createdAt: new Date(),
-//         avatar: 'https://facebook.github.io/react/img/logo_og.png',
-//         // https:'',
-//         user: {
-//           _id: 2,
-//           name: 'React Native',
-
-//           avatar: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
-//           // https: '',
-//         },
-//         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
-//         // You can also add a video prop:
-//         // video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-//         // Mark the message as sent, using one tick
-//         sent: true,
-//         // Mark the message as received, using two tick
-//         received: true,
-//         // Mark the message as pending with a clock loader
-//         pending: true,
-//         // Any additional custom parameters are passed through
-//       },
-//     ])
-//   }, [])
-//   const onSend = useCallback((messages = []) => {
-//     setMessages(previousMessages =>
-//       GiftedChat.append(previousMessages, messages)
-//     )
-//   }, [])
-//   return (
-//     <GiftedChat
-//       messages={messages}
-//       onSend={messages => onSend(messages)}
-//       user={{
-//         _id: 1,
-//       }}
-//     />
-//     // <View>
-//     //   <Text>MessageScreen</Text>
-//     // </View>
-//   )
+//     return (
+//         <GiftedChat
+//             messages={messages}
+//             showAvatarForEveryMessage={true}
+//             onSend={messages => onSend(messages)}
+//             user={{
+//                 _id: auth?.currentUser?.email,
+//                 name: auth?.currentUser?.displayName,
+//                 avatar: auth?.currentUser?.photoURL
+//             }}
+//         />
+//     );
 // }
 
-// export default MessageScreen
+// export default MessageScreen;
 
-// const styles = StyleSheet.create({})
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+
+const MessageScreen = () => {
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        name: 'React Native',
+        text: 'Hello developer',
+        createdAt: new Date(),
+        avatar: 'https://facebook.github.io/react/img/logo_og.png',
+        // https:'',
+        user: {
+          _id: 2,
+          name: 'React Native',
+
+          avatar: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
+          // https: '',
+        },
+        image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
+        // You can also add a video prop:
+        // video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+        // Mark the message as sent, using one tick
+        sent: true,
+        // Mark the message as received, using two tick
+        received: true,
+        // Mark the message as pending with a clock loader
+        pending: true,
+        // Any additional custom parameters are passed through
+      },
+    ])
+  }, [])
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages)
+    )
+  }, [])
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+    // <View>
+    //   <Text>MessageScreen</Text>
+    // </View>
+  )
+}
+
+export default MessageScreen
+
+const styles = StyleSheet.create({})
--- source/screens/ProfileScreen.js
+++ source/screens/ProfileScreen.js
@@ -1,7 +1,8 @@
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View, } from 'react-native'
-import React, {useState} from 'react'
+import { Image, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View, } from 'react-native'
+import React, { useState } from 'react'
 import theme from '../utils/styles'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import CustomProfileButton from '../components/CustomProfileButton'
 import CustomModal from '../components/CustomModal'
 import FAQsModal from '../components/FAQsModal'
@@ -16,33 +17,70 @@ const ProfileScreen = () => {
         setModalData('Are you sure to log out of your account?');
         setModalNameData('profileScreen')
         setModalVisible(true);
-      };
+    };
     const openFAQs = () => {
         setFAQsVisible(true);
-      };
-      const closeModal = () => {
+    };
+    const closeModal = () => {
         setModalVisible(false);
-        
-      }
-      const closeFAQs = () => {
+
+    }
+    const closeFAQs = () => {
         setFAQsVisible(false);
-       
-      }
+
+    }
+    const ProfileInfo = (props) => {
+        return (
+            <View style={styles.view1} onPress={props.onPress}>
+                <View style={styles.touchVIew}>
+                    <MaterialCommunityIcons
+                        name={props.name} size={29} color={theme.colors.primary} />
+                    <Text style={[styles.touchtext, { color: props.colors }]}>{props.Text}</Text>
+                    <Text style={styles.touchtext1}>{props.Text1}</Text>
+                </View>
+            </View>
+        );
+    };
     return (
         <View style={{ flex: 1, zIndex: 1 }}>
-            <View style={styles.firstView}>
-                <Image style={{alignSelf:'flex-end'}} source={require('../assets/images/Group.png')} />
-            </View>
+            <ImageBackground
+                resizeMode='stretch'
+                style={{ backgroundColor: 'rgba(25, 154, 142, 1.0)' }}
+                source={require('../assets/images/Group.png')}
+            >
+                <TouchableOpacity style={{ opacity: 9, alignSelf: 'flex-end', marginRight: 15, marginTop: 20, position: 'relative' }}>
+                    <Ionicons name={'ellipsis-vertical-sharp'} size={25} color={theme.colors.primary} style={{}} />
+                </TouchableOpacity>
+                <View style={styles.firstView}>
+                    {/* <Image style={{alignSelf:'flex-end'}} source={require('../assets/images/Group.png')} /> */}
+                    <View
+                        style={{ alignItems: 'center', justifyContent: 'flex-end' }}
+                    >
+                        <Image
+                            style={{ height: 110, width: 110 }}
+                            source={require('../assets/images/eallipse.png')} />
+                        <View style={{ backgroundColor: 'white', position: 'relative', left: 35, bottom: 28, padding: 3, borderRadius: 14 }}>
+                            <MaterialCommunityIcons name={'camera-outline'} size={20} color={'#199A8E'} style={{}} />
+                        </View>
+                        <Text style={{ fontFamily: theme.fonts.bold, color: theme.colors.primary, fontSize: 20 }} >Amelia Renata</Text>
+                        <View style={{ flexDirection: 'row' }}>
+                            <ProfileInfo name={'heart-pulse'} Text={'Heart rate'} colors={'#C1F3EF'} Text1={'215bpm'} />
+                            <ProfileInfo name={'fire'} Text={'Calories'} colors={'#C1F3EF'} Text1={'756cal'} />
+                            <ProfileInfo name={'dumbbell'} Text={'Weight'} colors={'#C1F3EF'} Text1={'103lbs'} />
+                        </View>
+                    </View>
+                </View>
+            </ImageBackground>
             <View style={styles.secondView}>
-                <CustomProfileButton name={'heart-outline'} Text={'My Saved'} color='rgba(25, 154, 142, 1)'  colors={theme.colors.tertiary}/>
-                <CustomProfileButton name={'document-text-outline'} Text={'Appointmnet'} color='rgba(25, 154, 142, 1)'  colors={theme.colors.tertiary}/>
-                <CustomProfileButton name={'wallet-outline'} Text={'Payment Method'} color='rgba(25, 154, 142, 1)'  colors={theme.colors.tertiary}/>
-                <CustomProfileButton name={'chatbubble-ellipses-outline'} Text={'FAQs'} color='rgba(25, 154, 142, 1)'  colors={theme.colors.tertiary} onPress={()=>openFAQs()} />
-                <CustomProfileButton name={'alert-circle-outline'} Text={'Logout'} color='rgba(255, 92, 92, 1)' colors='rgba(255, 92, 92, 1)' onPress={()=>openModal()} />
-                <FAQsModal visible={faqsVisible} closeModal={closeFAQs} 
+                <CustomProfileButton name={'heart-outline'} Text={'My Saved'} color='rgba(25, 154, 142, 1)' colors={theme.colors.tertiary} />
+                <CustomProfileButton name={'document-text-outline'} Text={'Appointmnet'} color='rgba(25, 154, 142, 1)' colors={theme.colors.tertiary} />
+                <CustomProfileButton name={'wallet-outline'} Text={'Payment Method'} color='rgba(25, 154, 142, 1)' colors={theme.colors.tertiary} />
+                <CustomProfileButton name={'chatbubble-ellipses-outline'} Text={'FAQs'} color='rgba(25, 154, 142, 1)' colors={theme.colors.tertiary} onPress={() => openFAQs()} />
+                <CustomProfileButton name={'alert-circle-outline'} Text={'Logout'} color='rgba(255, 92, 92, 1)' colors='rgba(255, 92, 92, 1)' onPress={() => openModal()} />
+                <FAQsModal visible={faqsVisible} closeModal={closeFAQs}
                 // customData={[modalNameData,modalData]} 
                 />
-                <CustomModal visible={modalVisible} closeModal={closeModal} customData={[modalNameData,modalData]}  />
+                <CustomModal visible={modalVisible} closeModal={closeModal} customData={[modalNameData, modalData]} />
             </View>
         </View>
 
@@ -57,8 +95,10 @@ const styles = StyleSheet.create({
     },
     firstView: {
         height: 350,
-        backgroundColor: 'rgba(25, 154, 142, 0.8)',
-        zIndex: 9
+        backgroundColor: 'rgba(25, 154, 142, 0.1)',
+        zIndex: 9,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     secondView: {
         backgroundColor: theme.colors.primary,
@@ -69,24 +109,32 @@ const styles = StyleSheet.create({
         bottom: 30,
         padding: 20
     },
-    touch: {
-        flexDirection: 'row',
+    view1: {
+        // flexDirection: 'row',
         justifyContent: 'space-between',
         marginHorizontal: 10,
-        alignItems: 'center'
+        alignItems: 'center',
+        marginVertical: 9,
+        padding: 5
     },
-    touchincircle: {
-        backgroundColor: 'rgba(232, 243, 241, 1)',
-        padding: 7,
-        borderRadius: 21,
-    },
-    touchinVIew: {
-        flexDirection: 'row',
+    touchVIew: {
+        borderRightColor: '#C1F3EF',
+        borderRightWidth: 1.5,
+        // flexDirection: 'row',
         alignItems: 'center',
     },
-    touchintext: {
-        color: 'rgba(255, 92, 92, 1)',
-        fontSize: 16,
+    touchtext: {
+        // color:props,
+        // color: 'rgba(255, 92, 92, 1)',
+        fontSize: 11,
+        // marginHorizontal: 10
+    },
+    touchtext1: {
+        // color:props,
+        // color: 'rgba(255, 92, 92, 1)',
+        color: theme.colors.primary,
+        fontSize: 15,
+        fontFamily: theme.fonts.bold,
         marginHorizontal: 10
-    }
+    },
 })
